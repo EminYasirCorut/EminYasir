e099dd9fb97a3e8a5cc9b8c2cd295cf45e204611
EminYasir <eminyasir.corut@stu.fsm.edu.tr>
2022-11-23 14:26:40 +0300

    eycak

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ images.length = 10;
 async function getData(){
     for(var i = 1 ; i < images.length ; i++){
         images[i] = new Image();
-      await  fetch('https://mehmetak7.github.io/mehmetak.github.io/HWimagess/'+'Dead (' + i.toString() + ').png').then(res=>res.blob())
+      await  fetch('https://mehmetak7.github.io/mehmetak.github.io/eyc/'+'Dead (' + i.toString() + ').png').then(res=>res.blob())
                                                  .then(blob => {
                                                     let objectURL = URL.createObjectURL(blob);
                                                     images[i].src = objectURL;
