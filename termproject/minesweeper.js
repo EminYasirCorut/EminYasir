3fa9a316b3fd7964f4492c23f72d05325fbc16d1
EminYasir <eminyasir.corut@stu.fsm.edu.tr>
2022-12-24 21:04:24 +0300

    last

--- termproject/minesweeper.js
+++ termproject/minesweeper.js
@@ -2,9 +2,9 @@ navigator.serviceWorker.register('sw.js');
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
 var row = 10;
-var column = 14;
+var column = 10;
 var mines = Math.floor(row * column / 10);
-var boxSize = 45;
+var boxSize = 40;
 
 var width = (row) * boxSize;
 var height = (column) * boxSize;
